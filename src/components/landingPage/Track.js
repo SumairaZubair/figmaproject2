c8d2b07ffce28e08d546f75dda0fb1afb28002a3
SumairaZubair <sumairazubair758@gmail.com>
2023-08-23 06:26:44 +0500

    first commit

--- src/components/landingPage/Track.js
+++ src/components/landingPage/Track.js
@@ -10,8 +10,12 @@ const Track = () => {
                         <h1>Track your business<br/> performance</h1>
                         <p>Use <span>Cottage</span> reports to figure out which menu items are selling the best and who your most frequent customers are</p>
                     </div>
+                    <button className='trackBtn'>GET STARTED</button>
                     <div className='trackImg'>
-                        <img className='trackImg' src={require('../../Assest/trackimg.png')} alt='track'/>
+                        {/* <img className='trackImg' src={require('../../Assest/trackimg.png')} alt='track'/> */}
+                    </div>
+                    <div className='trackimg'>
+
                     </div>
                 </div>
             </div>
