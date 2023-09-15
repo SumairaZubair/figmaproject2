98b3c11c7a51df4ac9ecb047366b306d732989a1
Sumaira Zubair <sumairazubair758@gmail.com>
2023-09-15 18:47:22 +0500

    first commit

--- src/authentication/verification/Verified.js
+++ src/authentication/verification/Verified.js
@@ -1,36 +1,32 @@
 
-import React,{useState} from 'react';
-import { useEffect } from 'react';
+
+import React, { useState, useEffect } from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { useNavigate } from 'react-router-dom';
-import { onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged, sendEmailVerification } from 'firebase/auth';
 import { auth } from '../fFirebaseConfig';
-import '../verification/verify.scss';
-import { message } from 'antd';
+import { message} from 'antd';
 
 const Verified = () => {
   const [confirmationMessage, setConfirmationMessage] = useState('');
- const navigate=useNavigate()
-const [emailVerified, setEmailVerified]=useState(true)
- 
+  const navigate = useNavigate();
+  const [emailVerified, setEmailVerified] = useState(true);
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+
       console.log(currentUser)
       if (currentUser) {
         if (currentUser.emailVerified) {
-         message.info('Email is verified');
-
-        } else {
-         message.info('Email is not verified');
-         setEmailVerified('')
+          navigate('/proAcc')
+          setEmailVerified(true);
         }
       }
     });
-  
+
     return () => unsubscribe();
   }, []);
-  
 
   const formik = useFormik({
     initialValues: {
@@ -41,64 +37,81 @@ const [emailVerified, setEmailVerified]=useState(true)
       email: Yup.string()
         .email('Invalid email address')
         .required('Email is required'),
-      code: Yup.string().required('Confirmation code is required'), 
+      code: Yup.string().required('Confirmation code is required'),
     }),
-    onSubmit: () => {
-      
-  // if (emailVerified) {
-  //   navigate('/proAcc');
-  // } else {
-  //   setConfirmationMessage('Email is not yet verified.');
-  // }
+    onSubmit: async (values) => {
+      // Send verification email
+      try {
+        await sendEmailVerification(auth.currentUser);
+        message.success('Verification email sent.');
+        navigate('/proAcc');
+      } catch (error) {
+        console.error(error);
+        message.error('Error sending verification email.');
+      }
     },
   });
-  const handleSubmit = (e) => {
-    e.preventDefault(); 
-    // formik.handleSubmit();
-    if (emailVerified) {
-      navigate('/proAcc');
-    } else {
-      setConfirmationMessage('Email is not yet verified.');
-    } 
-  };
 
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+
+  //   if (emailVerified=== true) {
+  //     // console.log('navigate')
+  //     navigate('/proAcc');
+  //   } else {
+  //     setConfirmationMessage('Email is not yet verified.');
+  //   }
+  // };
+
+  // Move the emailVerified update to useEffect
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        if (currentUser.emailVerified) {
+          setEmailVerified(true);
+          // navigate('/proAcc')
+        } else {
+          setEmailVerified(false);
+        }
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   return (
-    <div>
-      <div className='signUpContentV'>
-        <div className='signUpFormV'>
-          <img className='signLogoV' src={require('../../Assest/LOGO.png')} alt="Logo" />
-          <div className='formDivV'>
-            <div className='formV'>
-              <h1>Verification Code</h1>
-              <p>We emailed you a link please click on link and verify your email</p>
-                   <div>
-                    {emailVerified ?(<p className='verifyEmail'>Email verified now you can accessc your account</p>):
-                    <p className='verifyEmail'>Your email is not verified</p>
-                    }
-                   </div>
-              {/* <input
-                className='emailVv'
-                type="text"
-                placeholder="your one-time six-digit code"
-                value={confirmationCode}
-                onChange={handleConfirmationCodeChange}
-                required
-                name="code" 
-                {...formik.getFieldProps('code')} 
-              /> */}
-              {/* {formik.touched.code && formik.errors.code ? (
-                <div className="errorV">{formik.errors.code}</div>
-              ) : null} */}
 
-              <button className='verifyBtn' type="submit" onClick={handleSubmit} >Verify Now</button>
-              <p className='confirmMsg'>{confirmationMessage}</p>
-              <p className='ask'> Question? <span>We are here to help</span></p>
+    <div>
+    <div className="signUpContentV">
+      <div className="signUpFormV">
+        <img className="signLogoV" src={require('../../Assest/LOGO.png')} alt="Logo" />
+        <div className="formDivV">
+          <div className="formV">
+            <h1>Verification Code</h1>
+            <p>We emailed you a link; please click on the link to verify your email.</p>
+
+            <div>
+              {emailVerified ? (
+                
+                message.success('Your email has been successfully verified. You can now access your account.')
+              ) : (
+               
+                message.error('Your email is not yet verified. Please check your email for a verification link.')
+              )}
             </div>
+
+            <button className="verifyBtn" type="submit">
+              Verify Now
+            </button>
+            <p className="confirmMsg">{confirmationMessage}</p>
+            <p className="ask">
+              Question? <span>We are here to help</span>
+            </p>
           </div>
         </div>
       </div>
     </div>
+  </div>
   );
 };
 
